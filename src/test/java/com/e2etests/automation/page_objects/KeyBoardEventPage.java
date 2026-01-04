f28be2d3e8b5ab72dc72451e29ff4d8cf20ec2dd
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2026-01-04 16:44:02 +0100

    key-board-envent

--- src/test/java/com/e2etests/automation/page_objects/KeyBoardEventPage.java
+++ src/test/java/com/e2etests/automation/page_objects/KeyBoardEventPage.java
@@ -1,5 +1,62 @@
 package com.e2etests.automation.page_objects;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.e2etests.automation.utils.ConfigFileReader;
+import com.e2etests.automation.utils.Setup;
+
 public class KeyBoardEventPage {
+
+    private ConfigFileReader configFileReader;
+    private JavascriptExecutor executor;
+
+    @FindBy(how = How.ID, using = "userName")
+    private WebElement name;
+    @FindBy(how = How.ID, using = "userEmail")
+    private WebElement email;
+    @FindBy(how = How.ID, using = "currentAddress")
+    private WebElement currentAddress;
+    @FindBy(how = How.ID, using = "permanentAddress")
+    private WebElement permanentAddress;
+    @FindBy(how = How.ID, using = "submit")
+    private WebElement submitBtn;
+    @FindBy(how = How.XPATH, using = "//div[@class='border col-md-12 col-sm-12']")
+    private WebElement successMsg;
+
+     public KeyBoardEventPage(){
+        PageFactory.initElements(Setup.getDriver(), this);
+        this.configFileReader = new ConfigFileReader();
+        this.executor = (JavascriptExecutor) Setup.getDriver();
+     }
+
+     /** Methods **/
+     public void goToUrl(){
+        Setup.getDriver().get(configFileReader.getProperties("key-board-envent.url"));
+     }
+     public void fillFullName(){
+        name.sendKeys(configFileReader.getProperties("key-board-envent.name"));
+     }
+     public void fillEmail(){
+        email.sendKeys(configFileReader.getProperties("key-board-envent.email"));
+     }
+     public void filAddress(){
+        currentAddress.sendKeys(configFileReader.getProperties("key-board-envent.address"));
+     }
+     public void copyPasteAddress(){
+        currentAddress.sendKeys(Keys.CONTROL+"a");
+        currentAddress.sendKeys(Keys.CONTROL+"c");
+        permanentAddress.sendKeys(Keys.CONTROL+"v");
+     }
+     public void clickOnSubmit(){
+        executor.executeScript("arguments[0].click();", submitBtn);
+     }
+     public String getSuccessText(){
+       return successMsg.getText();
+     }
 
 }
--- src/test/java/com/e2etests/automation/step_definitions/KeyBoardEventStepDefinition.java
+++ src/test/java/com/e2etests/automation/step_definitions/KeyBoardEventStepDefinition.java
@@ -1,37 +1,52 @@
 package com.e2etests.automation.step_definitions;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.e2etests.automation.page_objects.KeyBoardEventPage;
+import com.e2etests.automation.utils.ConfigFileReader;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class KeyBoardEventStepDefinition {
 
+    KeyBoardEventPage keyBoardEventPage;
+    ConfigFileReader configFileReader;
+
+    public KeyBoardEventStepDefinition(){
+        this.keyBoardEventPage = new KeyBoardEventPage();
+        this.configFileReader = new ConfigFileReader();
+    }
+
 @Given("je suis dans la page text box")
 public void jeSuisDansLaPageTextBox() {
-
+    keyBoardEventPage.goToUrl();
 }
 @When("je saisie le nom complet")
 public void jeSaisieLeNomComplet() {
-
+    keyBoardEventPage.fillFullName();
 }
 @When("je saisie l email")
 public void jeSaisieLEmail() {
-    
+    keyBoardEventPage.fillEmail();
 }
 @When("je saisie l adresse")
 public void jeSaisieLAdresse() {
-    
+    keyBoardEventPage.filAddress();
 }
 @When("je copie l adresse")
 public void jeCopieLAdresse() {
-    
+    keyBoardEventPage.copyPasteAddress();
 }
 @When("je clique sur le bouton pour envoyer le formulaire")
 public void jeCliqueSurLeBoutonPourEnvoyerLeFormulaire() {
-    
+    keyBoardEventPage.clickOnSubmit();
 }
 @Then("le fomulaire est envoyé")
 public void leFomulaireEstEnvoyé() {
-    
+    	assertTrue(keyBoardEventPage.getSuccessText().contains(configFileReader.getProperties("key-board-envent.name")));
+        assertTrue(keyBoardEventPage.getSuccessText().contains(configFileReader.getProperties("key-board-envent.email")));
+        assertTrue(keyBoardEventPage.getSuccessText().contains(configFileReader.getProperties("key-board-envent.address")));
 }
 }
